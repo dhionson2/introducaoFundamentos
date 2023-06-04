75081a5a9254b69f94ed7c9e9bb85f0f4f1fcf1c
DHIONSON PLASTER MOREIRA <dhionsonmoreira@hotmail.com>
2023-06-03 22:25:59 -0300

    Primeiro componente e explicação de rotas simples

---
+++ fundamentos/src/pages/teste.jsx
@@ -0,0 +1,7 @@
+//se vc criar um arquivo dentro da pasta pages na url para vc acessar ele tera que digitar http://localhost:3000/teste ]
+//isso serve também se eu criar uma pasta dentro da pagina pages e criar outra dentro dela e outro arquivo
+//exemplo se dentro da pasta page eu criar uma pasta , teste, e dentro da teste outra pasta teste2, para eu acessar essa pagina
+//eu teria que digitar http://localhost:3000/teste/teste2/teste, assim eu acessaria essa pagina novamente
+export default function Teste() {
+    return "Teste"
+}
